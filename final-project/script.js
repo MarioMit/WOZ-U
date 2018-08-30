a26999a6255f568171f4eb432777e980978dfe20
Mario Mitchell <mario.mitchell8291@gmail.com>
2018-08-30 00:34:15 -0700

    CSS done.

--- final-project/script.js
+++ final-project/script.js
@@ -1,8 +1,8 @@
 function idCard(){
-    let firstName = getElementById("fName").value;
-    let lastName = getElementById("lName").value;
-    let address = getElementById("address").value;
-    let fullName = fName + lName;
+    let firstName = document.getElementById("first").value;
+    let lastName = document.getElementById("last").value;
+    let address = document.getElementById("address").value;
+    let fullName = firstName + " " +lastName;
     let age = parseInt(document.getElementById("years").value);
     let phoneN = parseInt(document.getElementById("phone").value);
     
@@ -14,14 +14,14 @@ function idCard(){
     let numberArray = [];
     numberArray.push(age, phoneN)
 
-    for (i = 0; i < numberArray; i++) {
-        if (numberArray <= 100) {
+         for (i = 0; i < numberArray.length; i++) {
+            if (numberArray <= 100) {
             document.getElementById("postAge").innerHTML = age;
         }
     }
-    for (i = 0; i < numberArray; i++) {
-        if (numberArray <= 100) {
-            ddocument.getElementById("postPhoneNumber").innerHTML = phoneN;
-    
-}
-
+        for (i = 0; i < numberArray.length; i++) {
+            if (numberArray <= 100) {
+            document.getElementById("postPhoneNumber").innerHTML = phoneN;
+        }
+    }
+    }
