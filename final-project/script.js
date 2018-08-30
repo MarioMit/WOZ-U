930327e83a2cfc5b9b74f64d5f5e2caabf6f8563
Mario Mitchell <mario.mitchell8291@gmail.com>
2018-08-30 10:45:26 -0700

    finished project

--- final-project/script.js
+++ final-project/script.js
@@ -3,25 +3,25 @@ function idCard(){
     let lastName = document.getElementById("last").value;
     let address = document.getElementById("address").value;
     let fullName = firstName + " " +lastName;
-    let age = parseInt(document.getElementById("years").value);
-    let phoneN = parseInt(document.getElementById("phone").value);
+    let age = parseInt(document.getElementById('years').value);
+    let phoneN = parseInt(document.getElementById('phone').value);
     
 
     document.getElementById("postFullName").innerHTML = fullName;
     document.getElementById("postAddress").innerHTML = address;
     
 
+    
     let numberArray = [];
-    numberArray.push(age, phoneN)
+    numberArray.push(age, phoneN);
 
-         for (i = 0; i < numberArray.length; i++) {
-            if (numberArray <= 100) {
-            document.getElementById("postAge").innerHTML = age;
-        }
+    for (i = 0; i < numberArray.length; i++) {
+        if (numberArray[i] <= 100) {
+        document.getElementById("postAge").innerHTML = age;
+        }else if (numberArray[i] > 100){ 
+        document.getElementById("postPhoneNumber").innerHTML = phoneN;
     }
-        for (i = 0; i < numberArray.length; i++) {
-            if (numberArray <= 100) {
-            document.getElementById("postPhoneNumber").innerHTML = phoneN;
-        }
-    }
-    }
+}
+    
+    
+}
